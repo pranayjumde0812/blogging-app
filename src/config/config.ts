f634baf28fd4bf12d9b0f619375532f48e7b5c80
Pranay Jumde <pranayjumde13@gmail.com>
2025-01-10 02:03:57 +0530

    valoidating the env variables with zod validation before use directly

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const ENUM_NODE_ENVS = z.enum(["production", "development", "staging"]);
+
+const envVarsSchema = z.object({
+  NODE_ENV: ENUM_NODE_ENVS,
+  PORT: z.coerce.number().default(3000),
+});
+
+export const env = envVarsSchema.parse(process.env);
