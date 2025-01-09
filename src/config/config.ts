bb21740b09865320fad4078b9bb8f8e1fc1c26a4
Pranay Jumde <pranayjumde13@gmail.com>
2025-01-10 02:24:12 +0530

    added mongo url for db connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,7 @@ const ENUM_NODE_ENVS = z.enum(["production", "development", "staging"]);
 const envVarsSchema = z.object({
   NODE_ENV: ENUM_NODE_ENVS,
   PORT: z.coerce.number().default(3000),
+  MONGODB_URL: z.string(),
 });
 
 export const env = envVarsSchema.parse(process.env);
