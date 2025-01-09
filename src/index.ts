b4b587faf6e95724d6e82e1772cbd1b13c2d11c6
Pranay Jumde <pranayjumde13@gmail.com>
2025-01-10 02:50:42 +0530

    database connection done successfully

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import express = require("express");
-import cors = require("cors");
+import express from "express";
+import cors from "cors";
 import { env } from "./config/config";
+import { databaseConnection } from "./config/dbconfig";
 
 const app = express();
+
+databaseConnection();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(env.PORT, () => {
-  console.log("app is running on ", env.PORT);
+  console.log("App is running on ", env.PORT);
 });
