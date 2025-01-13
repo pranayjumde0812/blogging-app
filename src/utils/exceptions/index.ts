5d35f89a9583c28dc0df72d9f8d34aa685cfca1a
Pranay Jumde <pranay.jumde@qodequay.com>
2025-01-13 15:07:41 +0530

    Exceptions Exported

--- src/utils/exceptions/index.ts
+++ src/utils/exceptions/index.ts
@@ -1,4 +1,13 @@
 import {BadRequestException} from './badRequest.exception';
+import {ForbiddenAccessException} from './forbiddenAccess.exception';
+import {InternalServerException} from './internalServer.exception';
 import {NotFoundException} from './notFound.exception';
+import {UnAuthorizedException} from './unauthorized.exception';
 
-export {BadRequestException, NotFoundException};
+export {
+  BadRequestException,
+  NotFoundException,
+  UnAuthorizedException,
+  ForbiddenAccessException,
+  InternalServerException,
+};
