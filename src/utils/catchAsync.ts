5d612829a397811bee266271af6b89d3d7267372
Pranay Jumde <pranay.jumde@qodequay.com>
2025-01-13 17:37:46 +0530

    created catchAsync to eliminate redundant try catch block

---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,11 @@
+import {Request, Response, NextFunction} from 'express';
+
+const catchAsync = (
+  fn: (req: any, res: Response, next: NextFunction) => Promise<void>,
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+export default catchAsync;
