2b6ca9b61969422a3394a3db4ddf1a5ad04f700d
Pranay Jumde <pranay.jumde@qodequay.com>
2025-01-13 16:04:55 +0530

    applied error handler

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import {env} from './config/config';
 import {databaseConnection} from './config/dbconfig';
 import logger from './utils/logger';
+import {errorHandler} from './utils/errorhandler';
 
 const app = express();
 
@@ -11,6 +12,9 @@ databaseConnection();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// Error handling middleware
+app.use(errorHandler);
 
 app.listen(env.PORT, () => {
   logger.info(`App is running on ${env.PORT}`);
